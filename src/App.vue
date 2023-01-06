fc8e9797d8713dfe0c30414383e16c8881466ec0
tugrankenger <turankenger@hotmail.com.tr>
2023-01-06 22:08:34 +0300

    setup vuelidate and validate email, password, repassord inputs and show error messages

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,43 @@
         <form @submit.prevent="onSubmit" style="width: 350px">
           <div class="form-group mb-3">
             <label>E-mail</label>
-            <input v-model="email" type="email" class="form-control" placeholder="Emnter your email address">
-            <small class="form-text text-muted">Error message</small>
+            <input
+            @blur="v$.$validate()"
+            v-model="email" 
+            type="email" 
+            class="form-control" 
+            :class="{'is-invalid': v$.email.$error}"
+            placeholder="Enter your email address">
+            <!-- <small class="form-text text-muted">Error message</small> -->
+            <div class="input-errors" v-for="error of v$.email.$errors" :key="error.$uid">
+              <div class="error-msg text-danger">{{ error.$message }}</div>
+            </div>
           </div>
           <div class="form-group mb-3">
             <label>Password</label>
-            <input v-model="password" type="password" class="form-control" placeholder="Enter your password">
+            <input 
+            @blur="v$.$validate()"
+            v-model="password" 
+            type="password" 
+            class="form-control" 
+            :class="{'is-invalid': v$.password.$error }"
+            placeholder="Enter your password">
+            <div class="input-errors" v-for="error of v$.password.$errors" :key="error.$uid">
+              <div class="error-msg text-danger">{{ error.$message }}</div>
+            </div>
           </div>
           <div class="form-group mb-3">
             <label>Password again</label>
-            <input v-model="repassword" type="password" class="form-control" placeholder="Enter your password again">
+            <input 
+            @blur="v$.$validate()"
+            v-model="repassword" 
+            type="password" 
+            class="form-control" 
+            :class="{'is-invalid': v$.repassword.$error}"
+            placeholder="Enter your password again">
+            <div class="input-errors" v-for="error of v$.repassword.$errors" :key="error.$uid">
+              <div class="error-msg text-danger">{{ error.$message }}</div>
+            </div>
           </div>
           <div class="form-group mb-3">
             <label> Select you want to register category </label>
@@ -36,18 +63,19 @@
         </form>
       </div>
       <div class="card p-4 my-3 shadow" style="width:400px">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aspernatur distinctio repellendus quia
-          cupiditate necessitatibus. Eius, est inventore vel provident sapiente dolore, libero deleniti, nihil sit
-          repellendus animi molestiae ullam.</p>
+        <p>{{ v$.repassword }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import {required, email, minLength, maxLength, sameAs} from '@vuelidate/validators'
+import { useVuelidate } from '@vuelidate/core'
 export default {
   data() {
     return {
+      v$: useVuelidate(),
       email: null,
       password: null,
       repassword: null,
@@ -73,7 +101,26 @@ export default {
       }
       this.hobbies.push(hobby)
     }
-  }
+  },
+  validations(){
+    return{
+      email:{
+        required,
+        email
+      },
+      password:{
+        required,
+        minLength: minLength(6),
+        maxLength: maxLength(8)
+      },
+      repassword:{
+        required,
+        minLength: minLength(6),
+        maxLength: maxLength(8),
+        sameAs: sameAs('password')
+      }
+    }
+  },
 }
 </script>
 
